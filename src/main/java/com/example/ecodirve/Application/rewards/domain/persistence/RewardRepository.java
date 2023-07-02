df600301d3d2499229991a8df8acaccc331a0842
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-02 12:56:44 -0500

    add reward repository

--- src/main/java/com/example/ecodirve/Application/rewards/domain/persistence/RewardRepository.java
+++ src/main/java/com/example/ecodirve/Application/rewards/domain/persistence/RewardRepository.java
@@ -3,6 +3,14 @@ package com.example.ecodirve.Application.rewards.domain.persistence;
 import com.example.ecodirve.Application.rewards.domain.model.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RewardRepository extends JpaRepository<Reward,Long> {
+
+    List<Reward> findAll();
+
+    Reward findByName(String name);
+    Reward findByScore(Double score);
+    Reward findByFleetId(Long fleetId);
 
 }
