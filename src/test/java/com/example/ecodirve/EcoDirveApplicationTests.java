59349943edd4ee8ce91267574571fc847ec33e85
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-02 11:41:27 -0500

    Initial commit

---
+++ src/test/java/com/example/ecodirve/EcoDirveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecodirve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDirveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
