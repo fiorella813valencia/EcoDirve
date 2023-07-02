61e49c64d747228dc5c1fadda607a9db0d5cd895
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-02 13:07:26 -0500

    add reward service interface

---
+++ src/main/java/com/example/ecodirve/Application/rewards/domain/service/RewardService.java
@@ -0,0 +1,19 @@
+package com.example.ecodirve.Application.rewards.domain.service;
+
+import com.example.ecodirve.Application.rewards.domain.model.Reward;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface RewardService {
+    List<Reward> getAll();
+    Page<Reward> getAll(Pageable pageable);
+    Reward getById(Long rewardId);
+    Reward create(Reward reward);
+    Reward update(Long rewardId,Reward request);
+
+    ResponseEntity<?> delete(Long rewardId);
+
+}
